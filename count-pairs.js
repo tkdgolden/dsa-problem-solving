b30143a2bd0f8eaba527ca82bc98858989edd850
Olivia Golden <olivia.golden.w@gmail.com>
2024-03-26 20:23:46 -0500

    count-pairs

--- count-pairs.js
+++ count-pairs.js
@@ -1,3 +1,29 @@
 // add whatever parameters you deem necessary
-function countPairs() {
+function countPairs(array, sum) {
+    // declare count
+    let count = 0;
+    // declare empty set
+    const set = new Set();
+
+    // iterate through array
+    for (let i of array) {
+        // find item difference
+        const diff = sum - i;
+        // if item difference equals item, continue loop
+        if (diff !== i) {
+            // if item difference in set
+            if (set.has(diff)) {
+                // increase count
+                count++;
+            }
+            else {
+                // else add item to set
+                set.add(i);
+            }
+        }
+    }
+
+    return count;
 }
+
+module.exports = countPairs;
