48fc492dfd78a675eee7ad9b6856f0609e626f9f
Olivia Golden <olivia.golden.w@gmail.com>
2024-03-26 19:34:55 -0500

    same-frequency

--- same-frequency.js
+++ same-frequency.js
@@ -1,3 +1,35 @@
-// add whatever parameters you deem necessary
-function sameFrequency() {
+function createFreq(int) {
+    // convert integer to string
+    const string = int.toString();
+    // declare map
+    const map = new Map();
+    // for digit in string
+    for (let digit of string) {
+        // get current entry value, default 0
+        const currVal = map.get(digit) || 0;
+        // increment current entry value or set to 1
+        map.set(digit, currVal + 1);
+    }
+
+    // return map object
+    return map;
 }
+
+
+// add whatever parameters you deem necessary
+function sameFrequency(int1, int2) {
+    // create frequency map for each integer
+    const map1 = createFreq(int1);
+    const map2 = createFreq(int2);
+
+    // for entry in map1
+    for (let digit of map1.keys()) {
+        // check val equals corresponding val in map2, return false if false
+        if (map1.get(digit) !== map2.get(digit)) return false;
+    }
+
+    // return default true, checked all
+    return true;
+}
+
+module.exports = sameFrequency;
--- same-frequency.test.js
+++ same-frequency.test.js
@@ -1,3 +1,5 @@
+const sameFrequency = require("./same-frequency")
+
 describe("sameFrequency", function() {
   it("should return true if the frequencies are the same", function() {
     expect(sameFrequency(182, 281)).toBe(true);
